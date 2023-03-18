47479aec71b7487e02868ba2e2e7966ff50cfa03
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-03-19 01:57:48 +0530

    updated

--- src/Day1.java
+++ src/Day1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Day1 {
 /*    -----------------------------
     primitive data types:
@@ -8,7 +10,23 @@ public class Day1 {
     5. long: 2^64/2 to 2^62/2 -1
     6. double:
     7. char: 0 to 65535(2^16-1)
+    8. Boolean: true/false; default: false
     ------------------------------
  */
+//    write a java program to add 3 nos.
+    public static int add(int a, int b, int c){
+        return a + b + c;
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first no.: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter second no.: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter third no.: ");
+        int c = scanner.nextInt();
+        System.out.println("The addition of "+ a + ", "+b+" and "+c+" is: ");
+        System.out.println(add(a,b,c));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the day: ");
-        int n = 1;
-        n = scanner.nextInt();
-        do {
-            day(n);
-        }while(n!=0);
-    }
-    public static void day(int n){
-        switch (n){
-            case 1:
-                Day1 day1 = new Day1();
-                break;
-            default:
-                System.out.println("Invalid Input! :(");
-                break;
-        }
+
     }
 }
