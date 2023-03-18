f064044757a0017fd19bb10f4be549aeca6dd713
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-03-19 01:47:48 +0530

    day1 intital

--- src/Day1.java
+++ src/Day1.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Day1 {
+public class Day1 {
+/*    ----------------------
+    primitive data types:
+    1. byte: -128 to 128
+    2. short: (2^16/2) to (2^16/2 - 1)
+    3. int: 2^32/2 to 2^32/2 - 1
+    4. float:
+    5. long: 2^64/2 to 2^62/2 -1
+    6. double:
+    7. char: 0 to 65535(2^16-1)
+ */
+
 }
